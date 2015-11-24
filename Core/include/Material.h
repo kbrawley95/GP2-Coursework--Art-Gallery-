4f752304ac82827b1dd6df3ebc97341f05a65b51
kbrawley95 <kieran.brawley95@gmail.com>
2015-11-24 11:27:59 +0000

    Material (Lighting Calculations Setup)

--- Core/include/Material.h
+++ Core/include/Material.h
@@ -3,6 +3,7 @@
 
 #include"Common.h"
 #include "Component.h"
+#include "Shader.h"
 
 class Material : public Component
 {
@@ -43,6 +44,31 @@ public:
 	~Material()
 	{
 
+	}
+
+	void CalculateLighting(shared_ptr<Light> light)
+	{
+		GLint ambientLightColourLocation = glGetUniformLocation(currentShaderProgram, "ambientLightColour");
+		GLint ambientMaterialColourLocation = glGetUniformLocation(currentShaderProgram, "ambientMaterialColour");
+
+		GLint diffuseLightColourLocation = glGetUniformLocation(currentShaderProgram, "diffuseLightColour");
+		GLint diffuseLightMaterialLocation = glGetUniformLocation(currentShaderProgram, "diffuseMaterialColour");
+		GLint lightDirectionLocation = glGetUniformLocation(currentShaderProgram, "lightDirection");
+
+		GLint specularLightColourLocation = glGetUniformLocation(currentShaderProgram, "specularLightColour");
+		GLint specularLightMaterialLocation = glGetUniformLocation(currentShaderProgram, "specularMaterialColour");
+		GLint specularPowerLocation = glGetUniformLocation(currentShaderProgram, "specularPower");
+
+		glUniform4fv(ambientLightColourLocation, 1, value_ptr(GetAmbientLightColour()));
+		glUniform4fv(ambientMaterialColourLocation, 1, value_ptr(GetAmbientMaterial()));
+
+		glUniform4fv(diffuseLightColourLocation, 1, value_ptr(GetDiffuseLightColour()));
+		glUniform4fv(diffuseLightMaterialLocation, 1, value_ptr(gameObject->GetDiffuseMaterial()));
+		glUniform3fv(lightDirectionLocation, 1, value_ptr(camera->GetLightDirection()));
+
+		glUniform4fv(specularLightColourLocation, 1, value_ptr(GetSpecularLightColour()));
+		glUniform4fv(specularLightMaterialLocation, 1, value_ptr(gameObject->GetSpecularMaterial()));
+		glUniform1f(specularPowerLocation, GetSpecularPower());
 	}
 
 private:
